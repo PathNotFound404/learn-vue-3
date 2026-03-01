55c1c31dfdb0d53287eac2b566043faff7f88d8f
PathNotFound404 <c.pinkston2004@gmail.com>
2026-02-28 22:29:25 -0600

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,58 @@
-<script setup></script>
-
 <template>
+  <h1>{{ message }}</h1>
+  <br />
+
+ 
+  <img v-bind:src="imageUrl" alt="" />
+  <br />
+  <!--Shorthand for v-bind-->
+  <img :src="imageUrl" alt="" />
+
+  <button @click="changeImg">Change image</button>
+  <hr/>
+  <input type="text" :value="defaultInputText">
+  <hr>
+  <p :class="className">Harry Potter</p>
+
+  <p :class="{ inactive: isInactive, center: isCenter }">Harry Potter</p>
+
+  <p :class="['active', 'center']">Harry Potter</p>
 
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from 'vue' 
+let message = 'Hello v-bind'
+
+let imageUrl = ref('/public/img/banner_1.jpg')
+
+function changeImg(){
+  imageUrl.value = '/public/img/banner_2.jpg'
+}
+
+let defaultInputText = 'Write something here'
+
+let className = 'active'
+
+let isInactive = ref(false)
+let isCenter = ref(false)
+</script>
+
+<style scoped>
+img {
+  max-width: 300px;
+}
+
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
+</style>
