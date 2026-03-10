735f6aa31141b6fc9a242edae1ab0e349e96dde6
Cody Pinkston <c.pinkston2004@gmail.com>
2026-03-10 17:01:53 -0500

    Introduction to view components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
-
 <template>
+    <ButtonCounter></ButtonCounter>
+    <ButtonCounter></ButtonCounter>
 
+    <button-counter></button-counter>
+
+    <hr>
+
+    <div style="background-color: orange; padding: 5px;">
+        This is the root componet
+        <ComponetA></ComponetA>
+        <ComponetA></ComponetA>
+    </div>
 </template>
+
+<script setup>
+    import ButtonCounter from './ButtonCounter.vue';
+    import ComponetA from '@/ComponetA.vue';
+</script>
 
 <style scoped></style>
---
+++ src/ComponetA.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="background-color: lightgreen; padding-left: 5px;">
+        This is the child componet
+        <CompentB></CompentB>
+        <CompentB></CompentB>
+        <CompentB></CompentB>
+    </div>
+</template>
+
+<script setup>
+import CompentB from './CompentB.vue';
+
+</script>
+
+<style scoped>
+
+</style>
