1a4757c34786b33fee814fdb24ccb94bab10c789
Cody Pinkston <c.pinkston2004@gmail.com>
2026-03-12 17:46:56 -0500

    pass template content to a component with slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
-
 <template>
-
+    <AlertBox>Invalid username or password. Please try again.</AlertBox>
+    <AlertBox></AlertBox>
+    <AlertBox>
+        <h1>The Request Timed out</h1>
+    </AlertBox>
 </template>
+
+<script setup>
+import AlertBox from './AlertBox.vue';
+
+</script>
 
 <style scoped></style>
