e4c630c55e0fe186fb3e8dfcd3a7907616354740
Cody Pinkston <c.pinkston2004@gmail.com>
2026-03-12 19:26:07 -0500

    vue named slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
-
 <template>
-
+    <BaseLayout>
+        <template v-slot:header>
+            <h1>Here might be a page title</h1>
+        </template>
+        <template #main>
+            <p>
+                A paragraph of the main
+            </p>
+            <p> And another</p>
+        </template>
+        <template #footer>
+            <p> Here is some contact info</p>
+        </template>
+    </BaseLayout>
 </template>
 
+<script setup>
+import BaseLayout from './BaseLayout.vue';
+</script>
 <style scoped></style>
