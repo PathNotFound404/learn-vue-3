893d54f4058564fb0a53eb9620bad7a6895eb7a2
Cody Pinkston <c.pinkston2004@gmail.com>
2026-03-13 17:26:35 -0500

    vue scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,65 @@
-<script setup></script>
-
 <template>
+    <div>
+        <StudentList :list="list">
+            <template #default="{ stu }">      <!-- # is the same as v-slot:  -->
+                <span :class="{cursed: stu.name == 'Harry'}">
+                    {{ stu.name }}
+                </span>
+            </template>
+        </StudentList>
 
+        <hr>
+
+        <el-table :data="todoList" stripe border style="width: 100%">
+            <el-table-column prop="userId" label="User ID" width="180" />
+            <el-table-column prop="id" label="ID" width="180" />
+            <el-table-column prop="title" label="Title" />
+            <el-table-column prop="completed" label="Status">
+                 <template #default="slotProps">
+                    <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+                    <el-tag type="danger" v-else>Incompleted</el-tag>
+                 </template>
+            </el-table-column>
+        </el-table>
+
+    </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import StudentList from './StudentList.vue';
+import { onMounted, ref } from 'vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+async function getTodoList(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    console.log(data)
+    todoList.value = data
+}
+
+onMounted(() => {
+    getTodoList()
+}) 
+</script>
+
+<style scoped>
+.cursed {
+    color: red;
+}
+</style>
---
+++ src/StudentList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <uL>
+            <li v-for="student in list" :key="student.id">
+                <slot :stu="student">
+                    {{ student.name }}
+                </slot>
+            </li>
+        </uL>
+    </div>
+</template>
+
+<script setup>
+defineProps(['list'])
+</script>
+
+<style scoped>
+
+</style>
